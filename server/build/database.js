31aaadbda727843315016f0d8ec8b2db3cda2f92
Pedro Mendoza <lpedromendoza333@gmail.com>
2019-10-06 16:24:13 -0500

    Cliente Pwa

--- server/build/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var mysql_1 = __importDefault(require("mysql"));
-var keys_1 = __importDefault(require("./keys"));
-var pool = mysql_1.default.createPool(keys_1.default.database);
-exports.default = pool;
--- server/src/database.ts
+++
@@ -1,6 +0,0 @@
-import mysql from 'mysql';
-import keys from './keys';
-
-const pool = mysql.createPool(keys.database);
-
-export default pool;
